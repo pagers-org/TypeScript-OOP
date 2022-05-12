a0c7141d3341976bf3ad75b6b82872746d1243a3
설아아빠 <dhrod0325@naver.com>
2022-05-12 22:41:23 +0900

    ✒️Feat: 유틸리티 함수 작성

---
+++ src/common/utils/utils.ts
@@ -0,0 +1,15 @@
+export function rangeRandom(start: number, end: number) {
+  return Math.floor(Math.random() * (end - start + 1) + start);
+}
+
+export function createElement(html: string): HTMLElement {
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.firstElementChild as HTMLElement;
+}
+
+export function createCustomElement(name: string, clazz: any) {
+  if (!customElements.get(name)) {
+    customElements.define(name, clazz);
+  }
+}
