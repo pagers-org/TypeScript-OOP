5ea74f21c1559c8f485601e8979efda01e829cdb
설아아빠 <dhrod0325@naver.com>
2022-05-12 22:42:10 +0900

    🧪Test: 음료,주문 테스트 코드 작성

---
+++ test/app.test.ts
@@ -0,0 +1,37 @@
+import { BeverageService, ApiImpl, Order, Orders, Recipe } from '@/domain';
+
+describe('app 테스트', () => {
+  const beverageService = new BeverageService(new ApiImpl());
+
+  describe('음료 테스트', () => {
+    it('음료 가저오기 테스트', () => {
+      const beverage = beverageService.getById(1);
+
+      expect(beverage?.name).toEqual('아메리카노');
+    });
+
+    it('레시피 가저오기 테스트', () => {
+      const recipes = beverageService.getRecipesByBeverageId(1);
+      expect(recipes).toEqual([new Recipe(1, 1, 1, 6), new Recipe(2, 1, 2, 4)]);
+    });
+  });
+
+  describe('주문 테스트', () => {
+    const orders = new Orders();
+
+    const order1 = new Order('1', 1);
+    const order2 = new Order('2', 2);
+    const order3 = new Order('3', 1);
+
+    orders.add(order1);
+    orders.add(order2);
+    orders.add(order3);
+
+    const getOrder1 = orders.getFirst(1);
+    expect(getOrder1?.id).toEqual('1');
+    if (getOrder1) orders.remove(getOrder1);
+
+    const getOrder2 = orders.getFirst(1);
+    expect(getOrder2?.id).toEqual('3');
+  });
+});
