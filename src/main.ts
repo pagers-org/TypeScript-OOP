55416eccffe1e4691c92249a7e09e450ffda604c
설아아빠 <dhrod0325@naver.com>
2022-05-12 22:43:52 +0900

    ✒️Feat: html 코드 수정

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,27 @@
-let currentElement: HTMLButtonElement | null = null;
-const pageNav = document.querySelector('header') as HTMLHeadElement;
-const coffeeName = document.querySelector('.coffee_name') as HTMLHeadingElement;
-const coffeeFilling = document.querySelector('.filling') as HTMLDivElement;
-const buttons = document.querySelectorAll<HTMLButtonElement>('.coffee-category-button');
-const addCoffeeOptionsForm = document.querySelector('.coffee-add-area form') as HTMLFormElement;
-const modalLayout = document.querySelector('.modal-layout') as HTMLDivElement;
+import { ApiImpl, BeverageService, MenuService, OptionService, Orders } from '@/domain';
+import { createCustomElement } from '@/common';
+import { MenuSelect } from '@/components/MenuSelect';
+import { OrderList } from '@/components/OrderList';
 
-pageNav.addEventListener('click', (event: MouseEvent) => {
+export const api = new ApiImpl();
+export const orders = new Orders();
+export const beverageService = new BeverageService(api);
+export const optionService = new OptionService(api);
+export const menuService = new MenuService(api);
+export const menu = menuService.getMenu();
+
+window.addEventListener('load', () => {
+  createCustomElement('menu-select', MenuSelect);
+  createCustomElement('order-list', OrderList);
+});
+
+export const modalLayout = document.querySelector('.modal-layout') as HTMLDivElement;
+
+document.querySelector('header')?.addEventListener('click', (event: MouseEvent) => {
   const $target = event.target as HTMLInputElement;
   if (!$target.matches('[type="radio"]')) return;
   event.preventDefault();
   alert('아직 준비되지 않았네요🥺');
-});
-
-buttons.forEach(button =>
-  button.addEventListener('click', () => {
-    if (currentElement) {
-      currentElement.classList.remove('selected');
-      coffeeFilling.classList.remove(currentElement.id);
-    }
-
-    currentElement = button;
-    coffeeFilling.classList.add(currentElement.id);
-    currentElement.classList.add('selected');
-    coffeeName.innerText = button.innerText;
-  }),
-);
-
-addCoffeeOptionsForm.addEventListener('submit', event => {
-  event.preventDefault();
-  modalLayout.classList.toggle('hidden');
 });
 
 modalLayout.addEventListener('click', (event: MouseEvent) => {
