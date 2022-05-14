19c22c663ccbfd0204464cd4588fe444df735c0c
Woo KyungJun <natcho9010@gmail.com>
2022-05-14 14:24:30 +0900

    Add : OptionProps in Option model

--- src/Model/Option.ts
+++ src/Model/Option.ts
@@ -1,4 +1,12 @@
-class Option {
+interface OptionProps {
+  name: string;
+  selections: string[];
+  type: 'radio' | 'checkbox';
+  isOptional?: boolean;
+  selectedIndex: number;
+}
+
+class Option implements OptionProps {
   // 옵션 이름
   name: string;
   // 사용 가능한 옵션
@@ -6,15 +14,16 @@ class Option {
   // 옵션 타입 (다중 선택, 하나 선택)
   type: 'radio' | 'checkbox';
   // 필수로 넣어야하는 옵션인가요?
-  isOptional: boolean;
+  isOptional?: boolean;
   // options 중에 선택된 것.
   selectedIndex = 0;
 
-  constructor({ name, selections, type, isOptional }: Option) {
+  constructor({ name, selections, type, isOptional, selectedIndex }: OptionProps) {
     this.name = name;
     this.selections = selections;
     this.type = type;
-    this.isOptional = isOptional;
+    this.isOptional = isOptional || false;
+    this.selectedIndex = selectedIndex || 0;
   }
 
   getOptions() {
