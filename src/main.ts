339119bac94d9417541dfb1f16d3ce1103a76416
Woo KyungJun <natcho9010@gmail.com>
2022-05-14 18:04:47 +0900

    Correct : add Views in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './View/OrderList/OrderList';
+import '@/View/OrderList/OrderList';
+import '@/View/Kitchen/Kitchen';
 
 let currentElement: HTMLButtonElement | null = null;
 const pageNav = document.querySelector('header') as HTMLHeadElement;
