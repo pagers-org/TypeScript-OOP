4ece4b5e1a20df014f9575e2939a5aa63a98b2a7
Woo KyungJun <natcho9010@gmail.com>
2022-05-14 15:27:23 +0900

    Add : DrinkList in Drink model

--- src/Model/Drink.ts
+++ src/Model/Drink.ts
@@ -1,8 +1,27 @@
 import Option from '@/Model/Option';
 
+/*
+  추후... 지원....
+  '카페오레',
+  '카푸치노',
+  '코레토',
+  '에스프레소',
+  '룽고',
+  '마끼아또',
+  '리스트레또',
+*/
+
+export const Drinks = [
+  '아메리카노',
+  '카페라떼',
+  '카페모카',
+] as const;
+
+export type DrinkList = typeof Drinks[number];
+
 // 마실 것만
 class Drink {
-  name: string;
+  name: DrinkList;
   options: Option[];
 
   constructor({ name, options }: Drink) {
