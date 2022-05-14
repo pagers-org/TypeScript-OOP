69ae34f80e59d1cdd856b8df52bd396f6946c31e
Woo KyungJun <natcho9010@gmail.com>
2022-05-14 14:25:53 +0900

    Update : OrderRow view, add update button event & remove HTMLElement extends

--- src/View/OrderList/OrderRow/OrderRow.ts
+++ src/View/OrderList/OrderRow/OrderRow.ts
@@ -1,29 +1,40 @@
 import Drink from '@/Model/Drink';
 
 // 메뉴 하나를 담당하는 view
-class OrderRow extends HTMLDivElement {
+// 한 줄을 만들어내는 컴포넌트에 가깝다.
+class OrderRow {
+  orderRowElement: HTMLDivElement;
   drinkRow: HTMLDivElement[] = [];
+  isEditMode = false;
 
-  constructor(drink: Drink) {
-    super();
-    this.setAttribute('class', 'table-row');
+  constructor(id: number, drink: Drink) {
+    this.orderRowElement = document.createElement('div');
+    this.orderRowElement.setAttribute('class', 'table-row');
 
-    const drinkName = this.createCell(drink.name, drink.name);
+    const idElement = this.createCell('id', id);
+    this.drinkRow.push(idElement);
+
+    const drinkName = this.createCell(drink.name, drink.name, true);
     this.drinkRow.push(drinkName);
 
     drink.getOptions().forEach(option => {
-      const optionCell = this.createCell(option.name, option.getSelectedOption());
+      const optionCell = this.createCell(option.name, option.getSelectedOption(), true);
       this.drinkRow.push(optionCell);
     });
 
     this.drinkRow.push(this.createButton('수정하기'));
     this.drinkRow.push(this.createButton('삭제하기'));
+
+    this.drinkRow.forEach(row => {
+      this.orderRowElement.appendChild(row);
+    });
   }
 
-  private createCell(title: string, content: string | number) {
+  private createCell(title: string, content: string | number, isEditable = false) {
     const newCell = document.createElement('div');
     newCell.setAttribute('class', 'cell');
-    newCell.setAttribute('data-title', 'cell');
+    newCell.setAttribute('data-title', title);
+    if (isEditable) newCell.classList.add('editable');
     newCell.textContent = String(content);
 
     return newCell;
@@ -32,7 +43,26 @@ class OrderRow extends HTMLDivElement {
   private getButtonAttribute = (type: string) => {
     if (type === '수정하기') {
       const listener = function (this: OrderRow) {
-        console.log(this);
+        const childrens = Array.from(this.orderRowElement.children);
+        let attributeFunction: (children: Element) => void;
+
+        if (!this.isEditMode) {
+          attributeFunction = (children: Element) => {
+            children.setAttribute('contenteditable', 'true');
+          };
+          this.isEditMode = true;
+        } else {
+          attributeFunction = (children: Element) => {
+            children.removeAttribute('contenteditable');
+          };
+          this.isEditMode = false;
+        }
+
+        childrens.forEach(children => {
+          if (children.classList.contains('editable')) {
+            attributeFunction(children);
+          }
+        });
       }.bind(this);
 
       return {
@@ -42,7 +72,7 @@ class OrderRow extends HTMLDivElement {
     }
 
     const listener = function (this: OrderRow) {
-      this.remove();
+      this.orderRowElement.remove();
     }.bind(this);
 
     return {
@@ -64,11 +94,15 @@ class OrderRow extends HTMLDivElement {
     iconEl.setAttribute('class', `fa-solid ${icon}`);
     span.appendChild(iconEl);
 
-    newButton.appendChild(newButton);
+    newButton.appendChild(span);
     newButton.addEventListener('click', listener);
 
     return newButton;
   };
+
+  render() {
+    return this.orderRowElement;
+  }
 }
 
 export default OrderRow;
