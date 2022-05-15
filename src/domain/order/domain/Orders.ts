de483024ae6d638a58bb4cdbfcef6aecd2a978d1
설아아빠 <dhrod0325@naver.com>
2022-05-15 23:40:54 +0900

    🗃Refactor: Orders.ts 리턴 타입 작성

--- src/domain/order/domain/Orders.ts
+++ src/domain/order/domain/Orders.ts
@@ -1,27 +1,27 @@
 import { Order } from '@/domain';
 
 export class Orders {
-  public orderData = new Map<number, Order[]>();
+  private orderData = new Map<number, Order[]>();
 
-  public add(order: Order) {
+  public add(order: Order): void {
     this.orderData.set(order.beverageId, [...this.getListByBeverageId(order.beverageId), order]);
   }
 
-  public remove(order: Order) {
+  public remove(order: Order): void {
     const filteredList = this.getListByBeverageId(order.beverageId).filter(item => item.id !== order.id);
     this.orderData.set(order.beverageId, [...filteredList]);
   }
 
-  public getListByBeverageId(beverageId: number) {
+  public getListByBeverageId(beverageId: number): Order[] {
     const result = this.orderData.get(beverageId);
     return result ? result : [];
   }
 
-  public getFirstByBeverageId(beverageId: number) {
+  public getFirstByBeverageId(beverageId: number): Order | undefined {
     return [...this.getListByBeverageId(beverageId)].shift();
   }
 
-  public getOrderValues() {
+  public getOrderValues(): Order[] {
     const orderRows: Order[][] = Array.from(this.orderData.values());
 
     return orderRows.reduce((list, orders) => {
@@ -29,15 +29,15 @@ export class Orders {
     }, []);
   }
 
-  public getOrderSize() {
+  public getOrderSize(): number {
     return this.getOrderValues().length;
   }
 
-  public isEmptyOrder() {
+  public isEmptyOrder(): boolean {
     return this.getOrderSize() == 0;
   }
 
-  public isEmptyByBeverageId(beverageId: number) {
+  public isEmptyByBeverageId(beverageId: number): boolean {
     return this.getListByBeverageId(beverageId).length == 0;
   }
 }
