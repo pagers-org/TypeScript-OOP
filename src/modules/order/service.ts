25a955a7cc9de6dd2a04e91df57418a28c9b9361
siyeon <sylee@megazone.com>
2022-05-15 23:30:06 +0900

    Feat: Order 클래스 생성 및 service 추가

---
+++ src/modules/order/service.ts
@@ -0,0 +1,29 @@
+import Order from '@/modules/order/index';
+import { OrderType } from '@/modules/order/type';
+
+export class OrderService {
+  public orderList: Order[] = [];
+
+  public add(order: Order) {
+    if (this.orderList.length === 0) this.orderList.push(order);
+    else this.orderList = [...this.orderList, order];
+  }
+
+  public delete(orderId: string) {
+    console.log('delete', orderId);
+    this.orderList = this.orderList.filter(item => item.id !== orderId);
+  }
+
+  public update(order: Order) {
+    this.orderList = this.orderList.map(item => {
+      if (item.id === order.id) {
+        return order;
+      }
+      return item;
+    });
+  }
+
+  public listOrders() {
+    return this.orderList;
+  }
+}
