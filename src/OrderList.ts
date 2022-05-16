1fa4b2a0596b4a2b0c23cf4931f7d5b423078c8d
정현수 <jung660317@naver.com>
2022-05-16 14:34:16 +0900

    🗃Refactor: template 폴더 생성 후 분리
    - order, kitchen template 생성

--- src/OrderList.ts
+++ src/OrderList.ts
@@ -2,6 +2,7 @@ import Order from './Order';
 
 import { DOM } from './constants';
 import { $ } from './utils/dom';
+import { ORDER_TEMPLATE } from './templates';
 
 import type { MenuName } from './@types';
 
@@ -72,51 +73,8 @@ class OrderList {
 
   renderOrderList() {
     this.$table.innerHTML =
-      this.orderTableHeaderTemplate() +
-      this.#orderList.map((order: Order) => this.orderTableRowTemplate(order)).join('');
-  }
-
-  orderTableRowTemplate(order: Order): string {
-    const { id, menuName, size, shot, syrup, ice, temporature, wippedCream, extra, cup }: Order = order;
-    return String.raw`
-      <div class="table-row" data-id="${id}">
-        <div class="cell" data-title="No">${id}</div>
-        <div class="cell" data-title="메뉴명">${menuName}</div>
-        <div class="cell" data-title="사이즈">${size}</div>
-        <div class="cell" data-title="샷">${shot}</div>
-        <div class="cell" data-title="시럽">${syrup}</div>
-        <div class="cell" data-title="ICE/HOT">${temporature}</div>
-        <div class="cell" data-title="얼음 종류">${ice}</div>
-        <div class="cell" data-title="휘핑 크림">${wippedCream}</div>
-        <div class="cell" data-title="엑스트라">${extra}</div>
-        <div class="cell" data-title="컵">${cup}</div>
-        <div class="cell" data-title="수정하기">
-          <span class="edit-order"><i class="fa-solid fa-pen"></i></span>
-        </div>
-        <div class="cell" data-title="삭제하기">
-          <span class="remove-order"><i class="fa-solid fa-trash-can"></i></span>
-        </div>
-      </div>
-    `;
-  }
-
-  orderTableHeaderTemplate(): string {
-    return String.raw`
-      <div class="table-row header">
-        <div class="cell">No</div>
-        <div class="cell">메뉴명</div>
-        <div class="cell">사이즈</div>
-        <div class="cell">샷</div>
-        <div class="cell">시럽</div>
-        <div class="cell">ICE/HOT</div>
-        <div class="cell">얼음 종류</div>
-        <div class="cell">휘핑 크림</div>
-        <div class="cell">엑스트라</div>
-        <div class="cell">컵</div>
-        <div class="cell">수정하기</div>
-        <div class="cell">삭제하기</div>
-      </div>
-    `;
+      ORDER_TEMPLATE.orderTableRowHeader() +
+      this.#orderList.map((order: Order) => ORDER_TEMPLATE.order(order)).join('');
   }
 }
 
---
+++ src/templates/kitchen.ts
@@ -0,0 +1,57 @@
+const KITCHEN_TEMPLATE = Object.freeze({
+  open() {
+    return String.raw`
+    <h1>주방</h1>
+    <div class="coffee-container">
+      <h1 class="coffee_name">Choose your coffee</h1>
+      <div class="cup">
+        <div class="filling reset">
+          <div class="coffee">커피</div>
+          <div class="water">물</div>
+          <div class="liquor">리퀴르</div>
+          <div class="milk">우유</div>
+          <div class="whipped_cream">휘핑 크림</div>
+          <div class="milk_foam">밀크 폼</div>
+          <div class="steamed_milk">데운 우유</div>
+          <div class="chocolate">초콜릿</div>
+        </div>
+        <div class="plate"></div>
+      </div>
+    </div>
+    <div class="select-coffee-container">
+      <div class="row">
+        <div>
+          <button class="coffee-category-button" id="americano">아메리카노</button>
+          <button class="coffee-category-button" id="au_lait">카페 오레</button>
+          <button class="coffee-category-button" id="capuccino">카푸치노</button>
+          <button class="coffee-category-button" id="corretto">코레또</button>
+          <button class="coffee-category-button" id="espresso">에스프레소</button>
+        </div>
+        <div>
+          <button class="coffee-category-button" id="latte">카페 라떼</button>
+          <button class="coffee-category-button" id="lungo">룽고</button>
+          <button class="coffee-category-button" id="macchiato">마끼야또</button>
+          <button class="coffee-category-button" id="mocha">카페 모카</button>
+          <button class="coffee-category-button" id="ristretto">리스트레또</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="coffee-add-area">
+          <form>
+            <button type="submit" class="coffee-add-options-button">만들기</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  `;
+  },
+  close() {
+    return String.raw`
+      <section id="right-section">
+        <div id="none-order"></div>
+      </section>
+    `;
+  },
+});
+
+export default KITCHEN_TEMPLATE;
