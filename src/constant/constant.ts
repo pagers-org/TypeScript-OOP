e35b0de11c7c954621d2f95321b7c804f425a694
설아아빠 <dhrod0325@naver.com>
2022-05-18 01:19:59 +0900

    🗃Refactor: 이벤트 정리

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,12 +1,85 @@
 export const EVENT = {
+  /**
+   * dispatchCustomEvent
+   * @link Component.constructor
+   */
   COMPONENT_INITIALIZE: 'Component.initialize',
+
+  /**
+   * dispatchCustomEvent
+   * @link OrderList.addOrder
+   *
+   * addCustomEventListener
+   * @link App.bindListeners
+   * @link Menu.bindListeners
+   */
   ORDER_ADDED: 'ORDER_ADDED',
+
+  /**
+   * dispatchCustomEvent
+   * @link Modal.bindEvents
+   * @link OrderListItem.removeOrder
+   *
+   * addCustomEventListener
+   * @link Menu.bindListeners
+   * @link App.bindListeners
+   */
   ORDER_REMOVED: 'ORDER_REMOVED',
-  ORDER_SUBMIT: 'ORDER_SUBMIT',
+
+  /**
+   * dispatchCustomEvent
+   * @link OrderListItem.removeOrder
+   *
+   * addCustomEventListener
+   * @link OrderList.bindListeners
+   */
   ORDER_LIST_ITEM_REMOVED: 'ORDER_LIST_ITEM_REMOVED',
+
+  /**
+   * dispatchCustomEvent
+   * @link Modal.bindEvents
+   *
+   * addCustomEventListener
+   * @link App.bindListeners
+   * @link OrderListItem.bindListeners
+   */
   CHANGE_OPTION: 'CHANGE_OPTION',
+
+  /**
+   * dispatchCustomEvent
+   * @link Modal.bindEvents
+   *
+   * addCustomEventListener
+   * @link OrderList.bindListeners
+   */
   SERVING: 'SERVING',
+
+  /**
+   * dispatchCustomEvent
+   * @link Modal.bindEvents
+   *
+   * addCustomEventListener
+   * @link App.bindListeners
+   * @link Served.bindListeners
+   */
   SERVED: 'SERVED',
+
+  /**
+   * dispatchCustomEvent
+   * @link Modal.open
+   * @link Modal.close
+   *
+   * addCustomEventListener
+   * @link App.bindListeners
+   */
   MODAL_OPEN: 'MODAL_OPEN',
+
+  /**
+   * dispatchCustomEvent
+   * @link MenuButton.bindEvents
+   *
+   * addCustomEventListener
+   * @link OrderList.bindListeners
+   */
   MENU_BUTTON_CLICK: 'MENU_BUTTON_CLICK',
 };
