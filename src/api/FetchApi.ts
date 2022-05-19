8ed59f17f07eca1fa6a06adbb4c982e577cc1674
설아아빠 <dhrod0325@naver.com>
2022-05-19 15:07:57 +0900

    🗃Refactor: FetchApi 타입 정의 및 코드 정리

--- src/api/FetchApi.ts
+++ src/api/FetchApi.ts
@@ -1,27 +1,62 @@
 import { AbstractApi } from '@/api/AbstractApi';
-import { Beverage, Material, Option, OptionGroup, Recipe } from '@/domain';
+import {
+  Beverage,
+  BeverageConstructor,
+  Material,
+  MaterialConstructor,
+  Option,
+  OptionConstructor,
+  OptionGroup,
+  OptionGroupConstructor,
+  Recipe,
+  RecipeConstructor,
+} from '@/domain';
 import { BeverageName, MaterialName, OptionGroupName } from '@/@types';
 
+export type FetchApiConstructor = {
+  host?: string;
+};
+
+const API_URL = {
+  OPTIONS: '/options.json',
+  BEVERAGES: '/beverages.json',
+  MATERIALS: '/materials.json',
+  OPTION_GROUPS: '/optionGroups.json',
+  RECIPES: '/recipes.json',
+} as const;
+
+type ApiUrlValueType = typeof API_URL[keyof typeof API_URL];
+
 export class FetchApi extends AbstractApi {
+  private readonly host: string;
+
+  constructor(constructor: FetchApiConstructor = {}) {
+    super();
+
+    this.host = constructor.host || '';
+  }
+
+  private async fetch<T>(url: ApiUrlValueType) {
+    const req = await fetch(`${this.host}${url}`);
+    return (await req.json()) as T[];
+  }
+
   protected async beverages(): Promise<Beverage[]> {
-    const req = await fetch('/beverages.json');
-    const res = (await req.json()) as any[];
+    const res = await this.fetch<BeverageConstructor>(API_URL.BEVERAGES);
     const result = res.map(item => new Beverage({ id: item.id, name: item.name as BeverageName }));
 
     return new Promise<Beverage[]>(resolve => resolve(result));
   }
 
   protected async materials(): Promise<Material[]> {
-    const req = await fetch('/materials.json');
-    const res = (await req.json()) as any[];
+    const res = await this.fetch<MaterialConstructor>(API_URL.MATERIALS);
     const result = res.map(item => new Material({ id: item.id, name: item.name as MaterialName }));
 
     return new Promise<Material[]>(resolve => resolve(result));
   }
 
   protected async optionGroups(): Promise<OptionGroup[]> {
-    const req = await fetch('/optionGroups.json');
-    const res = (await req.json()) as any[];
+    const res = await this.fetch<OptionGroupConstructor>(API_URL.OPTION_GROUPS);
     const optionsAll = await this.options();
 
     const result = res.map(item => {
@@ -39,16 +74,14 @@ export class FetchApi extends AbstractApi {
   }
 
   protected async options(): Promise<Option[]> {
-    const req = await fetch('/options.json');
-    const res = (await req.json()) as any[];
+    const res = await this.fetch<OptionConstructor>(API_URL.OPTIONS);
     const result = res.map(item => new Option({ id: item.id, optionGroupId: item.optionGroupId, name: item.name }));
 
     return new Promise<Option[]>(resolve => resolve(result));
   }
 
   protected async recipes(): Promise<Recipe[]> {
-    const req = await fetch('/recipes.json');
-    const res = (await req.json()) as any[];
+    const res = await this.fetch<RecipeConstructor>(API_URL.RECIPES);
     const result = res.map(
       item => new Recipe({ id: item.id, beverageId: item.beverageId, materialId: item.materialId, count: item.count }),
     );
