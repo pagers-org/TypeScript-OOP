40569e20d4a45c5d51ced550fccfd931f143c747
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-05-23 17:20:32 +0900

    🎆Style: CoffeeDTO 카멜 케이스로 수정

--- src/views/order.view.ts
+++ src/views/order.view.ts
@@ -5,15 +5,15 @@ export class OrderView {
   private orderTable = $("#order-table") as HTMLDivElement;
   createOrderRow(coffee: CoffeeDTO) {
     const content = `
-      <div class="table-row" data-key="1">
+      <div class="table-row" data-key=${coffee.id}>
         <div class="cell" data-title="No">${coffee.id}</div>
           <div class="cell" data-title="메뉴명">${coffee.name}</div>
           <div class="cell" data-title="사이즈">${coffee.size}</div>
           <div class="cell" data-title="샷">${coffee.shot}</div>
           <div class="cell" data-title="시럽">${coffee.syrup}</div>
-          <div class="cell" data-title="ICE/HOT">${coffee.icehot}</div>
+          <div class="cell" data-title="ICE/HOT">${coffee.iceHot}</div>
           <div class="cell" data-title="얼음 종류">${coffee.ice}</div>
-          <div class="cell" data-title="휘핑 크림">${coffee.whippedcream}</div>
+          <div class="cell" data-title="휘핑 크림">${coffee.whippedCream}</div>
           <div class="cell" data-title="엑스트라">${coffee.extra}</div>
           <div class="cell" data-title="컵">${coffee.cup}</div>
           <div class="cell" data-title="수정하기">
