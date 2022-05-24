4e7042b7a408ca278dede468424789fe4de0e666
Woo KyungJun <natcho9010@gmail.com>
2022-05-24 19:11:44 +0900

    Add : core Store

---
+++ src/core/Store.ts
@@ -0,0 +1,44 @@
+interface DisPatch {
+  type: string;
+  payload?: unknown;
+}
+
+function createStore<T>(name: string, callback: (store: T, action: DisPatch) => T, targetElement?: HTMLElement) {
+  let currentStore: T;
+  const reducer = callback;
+  const dispatchEventName = `${name}-dispatch`;
+  const listenerElement = targetElement || window;
+
+  listenerElement.addEventListener(dispatchEventName, (e: CustomEventInit<DisPatch>) => {
+    currentStore = reducer(currentStore, e.detail!);
+
+    const publish = new CustomEvent<T>(name, {
+      detail: currentStore,
+    });
+    dispatchEvent(publish);
+  });
+
+  function dispatch({ type, payload }: DisPatch) {
+    const dispatch = new CustomEvent(dispatchEventName, {
+      detail: {
+        type,
+        payload,
+      },
+    });
+    dispatchEvent(dispatch);
+  }
+
+  const getStore = () => {
+    return currentStore;
+  };
+
+  // init!
+  dispatch({ type: 'init' });
+
+  return {
+    dispatch,
+    getStore,
+  };
+}
+
+export default createStore;
