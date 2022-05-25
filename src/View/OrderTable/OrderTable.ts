31350dad46e32c56a1cdb83bf286c9d38d3473d0
Woo KyungJun <natcho9010@gmail.com>
2022-05-25 23:05:04 +0900

    Update : implement View in OrderTable view

--- src/View/OrderTable/OrderTable.ts
+++ src/View/OrderTable/OrderTable.ts
@@ -1,12 +1,13 @@
+import View from '@/core/View';
 import type OrderRow from '@/View/Components/OrderRow';
-import { dispatch } from '@/Stores/orderStore';
+import { dispatch } from '@/Stores/orderStore/orderStore';
 import OrderFactory from '@/Model/OrderFactory';
 
-class OrderTable {
+class OrderTable extends View {
   $orderTable = document.getElementById('order-table')!;
   $newOrderButton = document.getElementById('new-order')!;
 
-  bindEvent = () => {
+  protected bindEvents = () => {
     this.$newOrderButton.addEventListener('click', () => {
       const $order = document.createElement('cafe-order-row') as OrderRow;
       const orderFactory = new OrderFactory();
