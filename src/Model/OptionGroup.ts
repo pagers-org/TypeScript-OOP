86c1d9f45e5fe65c7fac3a39e01e2f75e2eff03f
Woo KyungJun <natcho9010@gmail.com>
2022-05-25 20:26:05 +0900

    Add : selectOption method in OptionGroup model

--- src/Model/OptionGroup.ts
+++ src/Model/OptionGroup.ts
@@ -10,6 +10,32 @@ class OptionGroup {
     this.options = options;
     this.isMultiple = isMultiple;
   }
+
+  private selectSingleOption = (optionIndex: number) => {
+    const newOption = this.options.map(option => {
+      option.setIsSelected(false);
+      return option;
+    });
+    newOption[optionIndex].setIsSelected(true);
+    this.options = newOption;
+  };
+
+  private selectMultipleOption = (optionIndex: number) => {
+    const targetOption = this.options[optionIndex];
+
+    if (targetOption.getIsSelected()) {
+      return targetOption.setIsSelected(false);
+    }
+    targetOption.setIsSelected(true);
+  };
+
+  selectOption = (optionIndex: number) => {
+    if (this.isMultiple) {
+      return this.selectMultipleOption(optionIndex);
+    }
+
+    this.selectSingleOption(optionIndex);
+  };
 }
 
 export default OptionGroup;
