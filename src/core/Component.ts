f6f7347b5e29c1a35132d07674739f93241e1ac0
Woo KyungJun <natcho9010@gmail.com>
2022-05-26 21:05:46 +0900

    Correct : delete comments

--- src/core/Component.ts
+++ src/core/Component.ts
@@ -3,9 +3,6 @@ export interface Template {
   children?: (Template | Element | Node | DocumentFragment)[];
 }
 
-// 안에서 html을 직접 바꿔주는 것을 전제로 개발한다. (react같은 component는 너무 어렵다...)
-// 외부의 데이터는 event를 통해 가져온다. (내 global redux를 발전시켜서 사용해보자.)
-// render 최적화(clone해서 거기에 변화사항을 적용하고 마는 거 같은)는 나중에,,,
 abstract class Component extends HTMLElement {
   constructor() {
     super();
@@ -32,9 +29,8 @@ abstract class Component extends HTMLElement {
     // override
   };
 
-  // 해당 컴포넌트의 렌더 과정은 모두 이곳에서
-  // 각자의 렌더링이 어떻게 될지는 알아서
-  // 사용자의 요청, 이걸 렌더해주세요!
+  // 각 컴포넌트의 렌더링이 어떻게 될지는 알아서
+  // 사용자 자유! 이 안에 렌더할 것을 넣어주세요!
   protected abstract template: () => Template;
 
   private composeComponents = (newTemplates: Template): Element | Node => {
@@ -52,7 +48,6 @@ abstract class Component extends HTMLElement {
   };
 
   // Component의 최종 결과물은 항상 이 함수를 통해야 한다.
-  // 이 안을 스스로 채워서 렌더링을 끝낸다.
   protected render = () => {
     const newTemplate = this.template();
     const rootElement = this.composeComponents(newTemplate);
