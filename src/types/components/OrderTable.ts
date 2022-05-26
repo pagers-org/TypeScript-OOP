8c594e5c2f9f420c76b9e1b96273be718ab50e78
dahye1013 <alrean@naver.com>
2022-05-26 18:35:06 +0900

    ✒️Feat: 주문 추가 상태변경에 따른 view 렌더링

--- src/types/components/OrderTable.ts
+++ src/types/components/OrderTable.ts
@@ -1,4 +1,6 @@
 import store from "../store";
+import Order from "../store/domain/Order";
+
 export default class OrderTableComponent extends HTMLElement {
   constructor(private readonly Store = store) {
     super();
@@ -7,8 +9,44 @@ export default class OrderTableComponent extends HTMLElement {
 
     const wrapper = document.createElement("div");
     this.shadowRoot.innerHTML = /* html */ `<link rel="stylesheet" href="./assets/css/index.css"></link>`;
+
     wrapper.setAttribute("class", "wrapper");
-    wrapper.innerHTML = /*html */ `
+    wrapper.innerHTML = this.getBaseMarkup();
+
+    wrapper.querySelector(".order-button")?.addEventListener("click", () => {
+      Store.dispatch("makeOrder");
+      this.renderOrderList();
+    });
+
+    this.shadowRoot.append(wrapper);
+  }
+
+  renderOrderList() {
+    const target = this.shadowRoot.querySelector(".table-row.header");
+
+    this.removePreStateOrders(target);
+    this.reRenderNewStateOrer(target);
+  }
+
+  removePreStateOrders = (target: HTMLElement) => {
+    if (target.nextElementSibling) {
+      while (target.nextElementSibling) {
+        target.nextElementSibling.remove();
+      }
+    }
+  };
+
+  reRenderNewStateOrer = (target: HTMLElement) => {
+    this.Store.state.orders.forEach((order: Order) => {
+      const tableRowElement = document.createElement("div");
+      tableRowElement.setAttribute("class", "table-row");
+      tableRowElement.innerHTML = this.getNewOrderMarkup(order);
+      target.parentElement.append(tableRowElement);
+    });
+  };
+
+  getBaseMarkup = () => {
+    return /*html */ `
                 <div class="order-list">
                     <h1>주문 목록</h1>
                     <div class="order-button-area">
@@ -30,32 +68,30 @@ export default class OrderTableComponent extends HTMLElement {
                                 <div class="cell">수정하기</div>
                                 <div class="cell">삭제하기</div>
                             </div>
-                            <div class="table-row">
-                                <div class="cell" data-title="No">1</div>
-                                <div class="cell" data-title="메뉴명">아메리카노</div>
-                                <div class="cell" data-title="사이즈">Tall</div>
-                                <div class="cell" data-title="샷">2</div>
-                                <div class="cell" data-title="시럽">-</div>
-                                <div class="cell" data-title="ICE/HOT">ICE</div>
-                                <div class="cell" data-title="얼음 종류">각얼음</div>
-                                <div class="cell" data-title="휘핑 크림">-</div>
-                                <div class="cell" data-title="엑스트라">-</div>
-                                <div class="cell" data-title="컵">1회용 컵</div>
-                                <div class="cell" data-title="수정하기">
-                                    <span class="edit-order"><i class="fa-solid fa-pen"></i></span>
-                                </div>
-                                <div class="cell" data-title="삭제하기">
-                                    <span class="remove-order"><i class="fa-solid fa-trash-can"></i></span>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>`;
+  };
 
-    wrapper.querySelector(".order-button")?.addEventListener("click", () => {
-      Store.dispatch("makeOrder");
-    });
-
-    this.shadowRoot.append(wrapper);
-  }
+  getNewOrderMarkup = (order: Order) => {
+    return /* html */ `
+            <div class="cell" data-title="No">0</div>
+            <div class="cell" data-title="메뉴명">${order.coffee.name}</div>
+            <div class="cell" data-title="사이즈">${order.size.name}</div>
+            <div class="cell" data-title="샷">${order.shot.name}</div>
+            <div class="cell" data-title="시럽">${order.syrup.name}</div>
+            <div class="cell" data-title="ICE/HOT">${order.iceOrHot.name}</div>
+            <div class="cell" data-title="얼음 종류">${order.ice.name}</div>
+            <div class="cell" data-title="휘핑 크림">${order.whippingCream.name}</div>
+            <div class="cell" data-title="엑스트라">${order.extra.name}</div>
+            <div class="cell" data-title="컵">${order.cup.name}</div>
+            <div class="cell" data-title="수정하기">
+                <span class="edit-order">수정</span>
+            </div>
+            <div class="cell" data-title="삭제하기">
+                <span class="remove-order">삭제</span>
+            </div>
+          `;
+  };
 }
